2b0ebf64c8bde7e0dd440294513bf1661c0b7483
Danielle Dees <d.danielle83@gmail.com>
2019-01-19 20:53:11 -0500

    update styling and javascript

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,3 +1,26 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+
+//click portfolio button to show images and links to work
+//click resume button to link to pdf 
+//click contact to show contact form
+
+$("#contact-form").hide()
+$(".port-content").hide()
+
+
+$("#contact-button").on("click", function() {
+    $("#contact-form").toggle()
+    $(".port-content").hide()
+    
+})
+
+$("#port-button").on("click", function() {
+    $(".port-content").toggle()
+    $("#contact-form").hide()
+
+})
+
+
+
 });
