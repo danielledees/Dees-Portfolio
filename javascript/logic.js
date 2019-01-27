2666a2b57e149844c0135b7d833b04fa3d128c54
Danielle Dees <d.danielle83@gmail.com>
2019-01-27 15:53:23 -0500

    cleaned up

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,17 +1,18 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-
+//hide contact and portfolio info until clicked
 $("#contact-form").hide()
 $(".port-content").hide()
 
-
+//show contact info and hide portfolo info
 $("#contact-button").on("click", function() {
     $("#contact-form").toggle()
     $(".port-content").hide()
     
 })
 
+//show portfolio info and hide contact info
 $("#port-button").on("click", function() {
     $(".port-content").toggle()
     $("#contact-form").hide()
