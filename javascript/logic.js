cac3a2bb2164fb7233c7cfbc95d83580f979e90c
Danielle Dees <d.danielle83@gmail.com>
2019-03-21 18:01:09 -0400

    project 2

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+$("#main").show()
 
 //hide contact and portfolio info until clicked
 $("#contact-form").hide()
@@ -7,17 +8,28 @@ $(".port-content").hide()
 
 //show contact info and hide portfolo info
 $("#contact-button").on("click", function() {
-    $("#contact-form").toggle()
+    $("#contact-form").show()
     $(".port-content").hide()
+    $("#main").hide()
     
 })
 
 //show portfolio info and hide contact info
 $("#port-button").on("click", function() {
-    $(".port-content").toggle()
+    $(".port-content").show()
     $("#contact-form").hide()
+    $("#main").hide()
 
 })
+
+$("#home").on("click", function() {
+    $("#main").show()
+    $(".port-content").hide()
+    $("#contact-form").hide()
+    
+})
+
+
 
 
 });
